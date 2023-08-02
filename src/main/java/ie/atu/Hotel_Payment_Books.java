111d7db73f9151afe7e2a6281e622b1fee2736b5
connorngouana <connorngouana@gmail.com>
2023-08-02 01:37:56 +0100

    added new clasees and populated them

--- src/main/java/ie/atu/Hotel_Payment_Books.java
+++ src/main/java/ie/atu/Hotel_Payment_Books.java
@@ -4,14 +4,81 @@ public class Hotel_Payment_Books
 {
     public int Length_of_Stay;
     public int Room_Rate;
-    public int Tax_Rate;
-    public int Discount_Rate;
+    public double Tax_Rate;
+    public String Discount_Code;
 
-    public Hotel_Payment_Books(int length_of_Stay, int room_Rate, int tax_Rate, int discount_Rate)
+    public Hotel_Payment_Books(int length_of_Stay, int room_Rate, double tax_Rate, String discount_Code)
     {
         this.Length_of_Stay = length_of_Stay;
         this.Room_Rate = room_Rate;
         this.Tax_Rate = tax_Rate;
-        this.Discount_Rate = discount_Rate;
+        this.Discount_Code = discount_Code;
     }
+
+    public int getLength_of_Stay()
+    {
+        return Length_of_Stay;
+    }
+
+    public void setLength_of_Stay(int length_of_Stay) {
+        if (Length_of_Stay >= 0 && Length_of_Stay <= 30) {
+            this.Length_of_Stay = length_of_Stay;
+        }
+        else
+        {
+            throw new IllegalArgumentException("You can only stay at the hotel 30 days at a time");
+        }
+    }
+    public int getRoom_Rate()
+    {
+        return Room_Rate;
+    }
+
+    public void setRoom_Rate(int room_Rate)
+    {
+        if (Room_Rate == 100)
+        {
+            this.Room_Rate = room_Rate;
+        }
+        else
+        {
+            throw new IllegalArgumentException("The standard rate of aa room is 100e");
+        }
+    }
+
+    public double getTax_Rate()
+    {
+        return Tax_Rate;
+    }
+
+    public void setTax_Rate(double tax_Rate)
+    {
+        if ( Tax_Rate == Length_of_Stay )
+        {
+            this.Tax_Rate = tax_Rate;
+        }
+        else
+        {
+            throw new IllegalArgumentException("The Tax goes up depending on the number of days you stay");
+        }
+    }
+
+    public String getDiscount_Code()
+    {
+        return Discount_Code;
+    }
+
+    public void setDiscount_Rate(String discount_Code)
+    {
+        if (Discount_Code == "JAVA" )
+        {
+            this.Discount_Code = discount_Code;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Invalid Discount code try again...");
+        }
+    }
+
 }
+
---
+++ src/test/java/ie/atu/Hotel_Payment_Books_Tests.java
@@ -0,0 +1,10 @@
+package ie.atu;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class Hotel_Payment_Books_Tests
+{
+    Hotel_User_Info_Test myH
+}
